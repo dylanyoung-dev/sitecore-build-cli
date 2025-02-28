0bbb087281b6d4322f6737c1525c157a976f1529
Dylan Young <5227854+dylanyoung-dev@users.noreply.github.com>
2025-02-28 15:15:03 -0500

    Initial CLI commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { readdirSync } from "fs";
+import { join } from "path";
+
+function main() {
+  const rootDir = process.cwd();
+  const files = readdirSync(rootDir);
+  console.log(`Files in ${rootDir}:`, files);
+}
+
+if (require.main === module) {
+  main();
+}
